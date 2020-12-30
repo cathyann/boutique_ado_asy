50a2c1c0414f0fc272495bc9c02059d1da181795
cathyann <cathyannsy@gmail.com>
2020-12-30 11:30:26 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51I421HGT7YR31uXW18rYSk05UktEv8IgC8SkFuSNIlM2A4xOWcI37h40X6py1HjpAyY4EqrxDBW4pEPWvt3eINg700wa4z7BO9',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
