c3d5da3ce230c609caca5191af489397d5086942
cathyann <cathyannsy@gmail.com>
2020-12-31 08:34:56 +0000

    Added checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -29,12 +29,10 @@ def webhook(request):
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print("Signature error")
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-
-    print("Success!")
-    return HttpResponse(status=200)    
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
